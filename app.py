48f27fe11d62eeec479a138182609a89d5764d9b
Shantanu Vishwanadha <mailvishwanadha@gmail.com>
2024-05-21 04:58:35 +0530

    Added 3 second repeat counter

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import logging
-
+import time
 from opentelemetry import trace
 from opentelemetry._logs import set_logger_provider
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import (
-    OTLPLogExporter,
-)
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-)
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
 
 # Define the instrumentation scope attributes
 instrumentation_scope = {
@@ -24,9 +19,7 @@ instrumentation_scope = {
 
 # Initialize tracer provider and logger provider
 trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(ConsoleSpanExporter())
-)
+trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
 
 logger_provider = LoggerProvider(
     resource=Resource.create(
@@ -70,22 +63,24 @@ class OTLPFormatter(logging.Formatter):
 for handler in root_logger.handlers:
     handler.setFormatter(OTLPFormatter())
 
-# Log directly
-logging.info("Jackdaws love my big sphinx of quartz.")
+# Function to generate logs every 3 seconds
+def generate_logs():
+    logger1 = logging.getLogger("myapp.area1")
+    logger2 = logging.getLogger("myapp.area2")
+    
+    while True:
+        logging.info("Jackdaws love my big sphinx of quartz.")
+        logger1.debug("Quick zephyrs blow, vexing daft Jim.")
+        logger1.info("How quickly daft jumping zebras vex.")
+        logger2.warning("Jail zesty vixen who grabbed pay from quack.")
+        logger2.error("The five boxing wizards jump quickly.")
+        
+        tracer = trace.get_tracer(__name__)
+        with tracer.start_as_current_span("foo"):
+            logger2.error("Hyderabad, we have a major problem.")
+        
+        time.sleep(3)
 
-# Create different namespaced loggers
-logger1 = logging.getLogger("myapp.area1")
-logger2 = logging.getLogger("myapp.area2")
-
-logger1.debug("Quick zephyrs blow, vexing daft Jim.")
-logger1.info("How quickly daft jumping zebras vex.")
-logger2.warning("Jail zesty vixen who grabbed pay from quack.")
-logger2.error("The five boxing wizards jump quickly.")
-
-# Trace context correlation
-tracer = trace.get_tracer(__name__)
-with tracer.start_as_current_span("foo"):
-    # Do something
-    logger2.error("Hyderabad, we have a major problem.")
-
-logger_provider.shutdown()
+if __name__ == "__main__":
+    generate_logs()
+    logger_provider.shutdown()
